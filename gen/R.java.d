c15e8e64d9fbd5bc0f7ce696d454ecfe38ae53c3
jessitron <jessitron@gmail.com>
2011-10-26 22:33:41 -0500

    Minimal input box

---
+++ gen/R.java.d
@@ -0,0 +1,7 @@
+C:\java\android\boyfriend\gen\com\jessitron\boyfriend\R.java \
+ : C:\java\android\boyfriend\res\drawable-hdpi\icon.png \
+C:\java\android\boyfriend\res\drawable-ldpi\icon.png \
+C:\java\android\boyfriend\res\drawable-mdpi\icon.png \
+C:\java\android\boyfriend\res\layout\main.xml \
+C:\java\android\boyfriend\res\values\strings.xml \
+C:\java\android\boyfriend\AndroidManifest.xml \
--- src/com/jessitron/boyfriend/BoyfriendActivity.java
+++ src/com/jessitron/boyfriend/BoyfriendActivity.java
@@ -2,14 +2,44 @@ package com.jessitron.boyfriend;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.text.TextWatcher;
+import android.text.Editable;
+import android.util.Log;
 
 public class BoyfriendActivity extends Activity
 {
+     private static final String LOG_PREFIX = "BoyfriendActivity";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        EditText phoneView = (EditText) findViewById(R.id.phoneNumber);
+  phoneView.addTextChangedListener(getTextChangedListener());
     }
+
+    public TextWatcher getTextChangedListener() {
+      return new TextWatcher() {
+        public void afterTextChanged(Editable s) {
+	  say("afterTextChanged " + s.getClass().getName());
+        }
+
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+	  say("before text changed " + s + count + after);
+        }
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+	  say("after text changed " + s + count + before);
+        }
+      };
+    }
+
+    private void say(String s)
+    {
+      Log.d(LOG_PREFIX, s);
+    }
+
 }
