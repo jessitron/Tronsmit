24eef0059f82cddda8156dee901ad5f4010df291
Jessica Kerr <jessica@gmail.com>
2012-05-21 20:54:54 -0500

    choose action multiplies

--- gen/com/jessitron/transmit/R.java
+++ gen/com/jessitron/transmit/R.java
@@ -14,13 +14,12 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int buttonContainer=0x7f060000;
-        public static final int history=0x7f060006;
-        public static final int otherTransmission=0x7f060004;
-        public static final int phoneNumber=0x7f060001;
-        public static final int pickContact=0x7f060002;
-        public static final int pictureView=0x7f060005;
-        public static final int transmit=0x7f060003;
+        public static final int buttonContainer=0x7f060001;
+        public static final int contactName=0x7f060002;
+        public static final int history=0x7f060005;
+        public static final int otherTransmission=0x7f060003;
+        public static final int pickContact=0x7f060000;
+        public static final int pictureView=0x7f060004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/jessitron/transmit/TransmitActivity.java
+++ src/com/jessitron/transmit/TransmitActivity.java
@@ -9,6 +9,8 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
@@ -23,16 +25,17 @@ import android.widget.Toast;
 
 public class TransmitActivity extends Activity {
     private static final int PICK_CONTACT_REQUEST_CODE = 1;
+    private static final int CHOOSE_INTENT_CODE = 2;
     private static final String LOG_PREFIX = "TransmitActivity";
 
-    private String phoneNumber;
-
     private PictureManager pictureManager;
-    private static final int CHOOSE_INTENT_CODE = 2;
-    private String email;
     private boolean htcDevice;
-    private Button chooseActionButton;
+
+    private String phoneNumber;
+    private String email;
     private String name;
+
+    private Button chooseActionButton;
 
     /**
      * Called when the activity is first created.
@@ -43,7 +46,6 @@ public class TransmitActivity extends Activity {
         setContentView(R.layout.main);
 
         loadPreferences();
-
         examineDevice();
 
         pictureManager = new PictureManager((ImageView) findViewById(R.id.pictureView), this);
@@ -51,15 +53,6 @@ public class TransmitActivity extends Activity {
         if (!pictureManager.hasPicture()) {
             disableAllButtons();
             toast("This app is useless without pictures");
-        }
-    }
-
-    private void disableAllButtons() {
-        LinearLayout container = (LinearLayout) findViewById(R.id.buttonContainer);
-        for(int i = 0; i < container.getChildCount(); i++) {
-            if (container.getChildAt(i) instanceof Button) {
-                container.getChildAt(i).setEnabled(false);
-            }
         }
     }
 
@@ -71,9 +64,194 @@ public class TransmitActivity extends Activity {
         say("HTC device? " + htcDevice);
     }
 
-    private void toast(String text) {
-        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+    private Intent createSendIntent(String action) {
+        Intent shareIntent = new Intent(action);
+        shareIntent.setType(pictureManager.getImageType());
+        shareIntent.addFlags(Intent.FLAG_DEBUG_LOG_RESOLUTION);
+
+        // everything needs the data
+        shareIntent.putExtra(Intent.EXTRA_STREAM, pictureManager.getImageLocation());
+
+        // MMS on non-HTC
+        shareIntent.putExtra(Intent.EXTRA_PHONE_NUMBER, phoneNumber);
+
+        // HTC
+        shareIntent.putExtra("address", phoneNumber);
+        shareIntent.putExtra("sms_body", "sent by Transmitron");
+
+        // Email
+        if (email != null) {
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
+        }
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "sent by Transmitron");
+
+        return shareIntent;
     }
+
+
+    public void pictureTouched(View v) {
+        Toast.makeText(this, "you touched me", 3).show();
+        pictureManager.advance();
+    }
+
+    public void sendPicture(View v) {
+        Intent shareIntent = createSendIntent(Intent.ACTION_SEND);
+
+        startActivity(shareIntent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        say("onResume called");
+        pictureManager.reset();
+    }
+
+    private void savePreferences(Uri contactUri) {
+        getPreferences(MODE_PRIVATE).edit().putString("contactUri", contactUri.toString()).apply();
+    }
+
+    private void loadPreferences() {
+        String uriString = getPreferences(MODE_PRIVATE).getString("contactUri", "");
+        if ("" != uriString) {
+            gotAContact(Uri.parse(uriString));
+        }
+        updateContactDescription();
+    }
+
+    private void updateContactDescription() {
+        TextView contactDescription = (TextView) findViewById(R.id.contactName);
+        contactDescription.setText(name);
+        contactDescription.invalidate();
+    }
+
+    public void pickContact(View v) {
+        final Intent pickContactsIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        ResolveInfo resolved = getPackageManager().resolveActivity(pickContactsIntent, 0);
+        say("pick contact resolves to: " + resolved.activityInfo.name);
+        startActivityForResult(pickContactsIntent, PICK_CONTACT_REQUEST_CODE);
+    }
+
+    public void chooseAction(View v) {
+        //final Intent pickActivityIntent = Intent.createChooser(createSendIntent(Intent.ACTION_SEND), "What should this button do?");
+        final Intent pickActivityIntent = new Intent(Intent.ACTION_PICK_ACTIVITY);
+        pickActivityIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        pickActivityIntent.putExtra(Intent.EXTRA_INTENT, createSendIntent(Intent.ACTION_SEND));
+        pickActivityIntent.putExtra(Intent.EXTRA_TITLE, "what should this button do?");
+        chooseActionButton = (Button) v;
+        startActivityForResult(pickActivityIntent, CHOOSE_INTENT_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
+        if (requestCode == PICK_CONTACT_REQUEST_CODE && resultCode == RESULT_OK) {
+            if (resultCode == RESULT_OK) {
+                gotAContact(data.getData());
+                savePreferences(data.getData());
+            }
+        } else if (requestCode == CHOOSE_INTENT_CODE && resultCode == RESULT_OK) {
+            gotAnAction(data);
+            addAbutton();
+        }
+    }
+
+    private void addAbutton() {
+        final LinearLayout buttonContainer = findButtonContainer();
+        final Button newButton = new Button(this);
+        newButton.setText("Choose an action");
+        newButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseAction(view);
+            }
+        });
+        buttonContainer.addView(newButton);
+    }
+
+    private void gotAnAction(final Intent data) {
+        // note: this might not be a default action. could be trouble.
+        chooseActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityLike(data);
+            }
+        });
+        ActivityInfo info = data.resolveActivityInfo(getPackageManager(), 0);
+        chooseActionButton.setText("Send to " + info.loadLabel(getPackageManager()));
+    }
+
+    private void startActivityLike(final Intent data) {
+        Intent send = createSendIntent(data.getAction());
+        send.setComponent(data.getComponent());
+        startActivity(send);
+    }
+
+    private void gotAContact(android.net.Uri uri) {
+
+        long contactId = loadNameAndId(uri);
+
+        loadPhoneNumber(contactId);
+
+        loadEmail(contactId);
+
+        updateContactDescription();
+    }
+
+    private long loadNameAndId(Uri uri) {
+        long contactId = -1;
+        name = "Nobody";
+        Cursor cursor = getContentResolver().query(uri,
+                new String[]{ContactsContract.Contacts._ID,
+                        ContactsContract.Contacts.DISPLAY_NAME},
+                null, null, null);
+        try {
+            if (cursor.moveToFirst()) {
+                contactId = cursor.getLong(0);
+                name = cursor.getString(1);
+            } else {
+                say("Contact not found");
+            }
+        } finally {
+            cursor.close();
+        }
+        say("contactId is " + contactId);
+        return contactId;
+    }
+
+    private void loadEmail(long contactId) {
+        email = null;
+        Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                new String[]{ContactsContract.CommonDataKinds.Email.DATA1},
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + contactId, null, ContactsContract.CommonDataKinds.Phone.IS_SUPER_PRIMARY + " DESC");
+        try {
+            if (cursor.moveToFirst()) {
+                email = cursor.getString(0);
+                say("Chose email: " + email);
+            } else {
+                say("Email not found");
+            }
+        } finally {
+            cursor.close();
+        }
+    }
+
+    private void loadPhoneNumber(long contactId) {
+        phoneNumber = null;
+        Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + contactId, null, ContactsContract.CommonDataKinds.Phone.IS_SUPER_PRIMARY + " DESC");
+        try {
+            if (cursor.moveToFirst()) {
+                phoneNumber = cursor.getString(0);
+                say("Chose phone number: " + phoneNumber);
+            } else {
+                say("Phone number not found");
+            }
+        } finally {
+            cursor.close();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -89,6 +267,37 @@ public class TransmitActivity extends Activity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void disableAllButtons() {
+        LinearLayout container = findButtonContainer();
+        for (int i = 0; i < container.getChildCount(); i++) {
+            if (container.getChildAt(i) instanceof Button) {
+                container.getChildAt(i).setEnabled(false);
+            }
+        }
+    }
+
+    private LinearLayout findButtonContainer() {
+        return (LinearLayout) findViewById(R.id.buttonContainer);
+    }
+
+    public void sendPictureHtc(View v) {
+        Intent shareIntent = new Intent("android.intent.action.SEND_MSG");
+        shareIntent.putExtra(Intent.EXTRA_STREAM, pictureManager.getImageLocation());
+        shareIntent.putExtra("address", phoneNumber);
+        shareIntent.putExtra("sms_body", "sent by Transmit");
+        shareIntent.setType(pictureManager.getImageType());
+
+        startActivity(shareIntent);
+    }
+
+    private void say(String s) {
+        Log.d(LOG_PREFIX, "JessiTRON! " + s);
+    }
+
+    private void toast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
     }
 
     private void printStuff() {
@@ -115,18 +324,6 @@ public class TransmitActivity extends Activity {
 
     }
 
-    private Intent createSendIntent(String action) {
-        Intent shareIntent = new Intent(action);
-        shareIntent.addFlags(Intent.FLAG_DEBUG_LOG_RESOLUTION);
-        shareIntent.putExtra(Intent.EXTRA_STREAM, pictureManager.getImageLocation());
-        shareIntent.putExtra(Intent.EXTRA_PHONE_NUMBER, phoneNumber);
-        shareIntent.putExtra("address", phoneNumber);
-        if (email != null) { shareIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {email}); }
-
-        shareIntent.putExtra("sms_body", "sent by Transmitron");
-        shareIntent.setType(pictureManager.getImageType());
-        return shareIntent;
-    }
 
     private void printInfoAboutAllApplications() {
         say("========================");
@@ -151,164 +348,4 @@ public class TransmitActivity extends Activity {
             }
         }
     }
-
-    public void pictureTouched(View v) {
-        Toast.makeText(this, "you touched me", 3).show();
-        pictureManager.advance();
-    }
-
-    public void sendPicture(View v) {
-        Intent shareIntent = createSendIntent(Intent.ACTION_SEND);
-
-        startActivity(shareIntent);
-    }
-
-    public void sendPictureHtc(View v) {
-        Intent shareIntent = new Intent("android.intent.action.SEND_MSG");
-        shareIntent.putExtra(Intent.EXTRA_STREAM, pictureManager.getImageLocation());
-        shareIntent.putExtra("address", phoneNumber);
-        shareIntent.putExtra("sms_body", "sent by Transmit");
-        shareIntent.setType(pictureManager.getImageType());
-
-        startActivity(shareIntent);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        pictureManager.reset();
-    }
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        savePreferences();
-    }
-
-    private void savePreferences() {
-        getPreferences(MODE_PRIVATE).edit().putString("phoneNumber", phoneNumber).apply();
-    }
-
-    private void loadPreferences() {
-        setPhoneNumber(getPhoneNumber());
-    }
-
-    private String getPhoneNumber() {
-        return getPreferences(MODE_PRIVATE).getString("phoneNumber", "");
-    }
-
-    private void setPhoneNumber(String phoneNumber) {
-        TextView phoneView = (TextView) findViewById(R.id.phoneNumber);
-        phoneView.setText(phoneNumber);
-        phoneView.invalidate();
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void pickContact(View v) {
-        final Intent pickContactsIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-        ResolveInfo resolved = getPackageManager().resolveActivity(pickContactsIntent, 0);
-        say("pick contact resolves to: " + resolved.activityInfo.name);
-        startActivityForResult(pickContactsIntent, PICK_CONTACT_REQUEST_CODE);
-    }
-
-    public void chooseAction(View v) {
-        //final Intent pickActivityIntent = Intent.createChooser(createSendIntent(Intent.ACTION_SEND), "What should this button do?");
-        final Intent pickActivityIntent = new Intent(Intent.ACTION_PICK_ACTIVITY);
-        pickActivityIntent.putExtra(Intent.EXTRA_INTENT, createSendIntent(Intent.ACTION_SEND));
-        pickActivityIntent.putExtra(Intent.EXTRA_TITLE, "what should this button do?");
-        chooseActionButton = (Button) v;
-        startActivityForResult(pickActivityIntent, CHOOSE_INTENT_CODE);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
-        if (requestCode == PICK_CONTACT_REQUEST_CODE && resultCode == RESULT_OK) {
-           if (resultCode == RESULT_OK) {
-            gotAContact(data.getData());
-           }
-        } else if (requestCode == CHOOSE_INTENT_CODE && resultCode == RESULT_OK) {
-            gotAnAction(data);
-        }
-    }
-
-    private void gotAnAction(final Intent data) {
-        // note: this might not be a default action. could be trouble.
-        chooseActionButton.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivityLike(data);
-            }
-        });
-        // change the text on the button.
-        ActivityInfo info = data.resolveActivityInfo(getPackageManager(), 0);
-        chooseActionButton.setText("Send to " + info.loadLabel(getPackageManager()));
-    }
-
-    private void startActivityLike(final Intent data) {
-        Intent send = createSendIntent(data.getAction());
-        send.setComponent(data.getComponent());
-        startActivity(send);
-    }
-
-    private void gotAContact(android.net.Uri uri) {
-
-        long contactId = -1;
-
-        // Load the display name for the specified person
-        android.database.Cursor cursor = getContentResolver().query(uri,
-                new String[]{ContactsContract.Contacts._ID,
-                        ContactsContract.Contacts.DISPLAY_NAME},
-                null, null, null);
-        try {
-            if (cursor.moveToFirst()) {
-                contactId = cursor.getLong(0);
-                name = cursor.getString(1);
-            } else {
-                say("Contact not found");
-            }
-        } finally {
-            cursor.close();
-        }
-        say("contactId is " + contactId);
-
-        // Load the phone number (if any).
-        cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
-                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + contactId, null, ContactsContract.CommonDataKinds.Phone.IS_SUPER_PRIMARY + " DESC");
-        try {
-            if (cursor.moveToFirst()) {
-                setPhoneNumber(cursor.getString(0));
-                say("Chose phone number: " + phoneNumber);
-            } else {
-                phoneNumber = null;
-                say("Phone number not found");
-            }
-        } finally {
-            cursor.close();
-        }
-
-        // look for an email
-        cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,
-                new String[]{ContactsContract.CommonDataKinds.Email.DATA1},
-                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + contactId, null, ContactsContract.CommonDataKinds.Phone.IS_SUPER_PRIMARY + " DESC");
-        try {
-            if (cursor.moveToFirst()) {
-               email = cursor.getString(0);
-                say("Chose email: " + email);
-            } else {
-                email = null;
-                say("Email not found");
-            }
-        } finally {
-            cursor.close();
-        }
-
-    }
-
-
-    private void say(String s) {
-        Log.d(LOG_PREFIX, "JessiTRON! " + s);
-    }
-
 }
